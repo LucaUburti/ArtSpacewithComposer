d9cd9d21a48cb6cc7c543dfcac4b46d334677844
uburti <luca.uburti@gmail.com>
2023-02-21 00:46:56 +0100

    Logic added, seems to be working fine :)

--- app/src/main/java/com/example/artspacewithcompose/MainActivity.kt
+++ app/src/main/java/com/example/artspacewithcompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.artspacewithcompose
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,7 +12,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,15 +31,92 @@ class MainActivity : ComponentActivity() {
             ArtSpaceWithComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    ArtSpaceScreen()
+                    val pictureList = populatePictureList()
+                    println("arraylist size: " + pictureList.size)
+                    ArtSpaceScreen(pictureList)
                 }
             }
         }
     }
 }
 
+fun populatePictureList(): ArrayList<PictureData> {
+    val pictureList = ArrayList<PictureData>()
+    pictureList.add(
+        PictureData(
+            R.drawable.apples,
+            "Apples turning red",
+            "Wheewee Pottyhall",
+            (2000..2023).random()
+        )
+    )
+    pictureList.add(PictureData(R.drawable.cat, "Curious cat", "Hicktu Sockface", (2000..2023).random()))
+    pictureList.add(
+        PictureData(
+            R.drawable.dandelion,
+            "Dandelion in high winds",
+            "Gumtu Spottybrain",
+            (2000..2023).random()
+        )
+    )
+    pictureList.add(
+        PictureData(
+            R.drawable.dragonfly,
+            "Precarious Dragonfly",
+            "Sniffbuns Hippyhall",
+            (2000..2023).random()
+        )
+    )
+    pictureList.add(
+        PictureData(
+            R.drawable.monkeys,
+            "Eyebrowns pulling Monkeys",
+            "Fluffmoo AntFadden",
+            (2000..2023).random()
+        )
+    )
+    pictureList.add(
+        PictureData(
+            R.drawable.poppies,
+            "Psychedelic Poppies",
+            "Poorster PimpleSmittens",
+            (2000..2023).random()
+        )
+    )
+    pictureList.add(
+        PictureData(
+            R.drawable.streetcar,
+            "Suicide by streetcar",
+            "Bittypants Hippykins",
+            (2000..2023).random()
+        )
+    )
+    pictureList.add(
+        PictureData(
+            R.drawable.watch,
+            "Can't wait to read this",
+            "Poopaloo Oinkkins",
+            (2000..2023).random()
+        )
+    )
+    pictureList.add(
+        PictureData(
+            R.drawable.zebra,
+            "Zebra minding its own business",
+            "Captain Shenanigans",
+            (2000..2023).random()
+        )
+    )
+    return pictureList
+}
+
+data class PictureData(val imgRes: Int, val title: String, val artist: String, val year: Int)
+
 @Composable
-fun ArtSpaceScreen() {
+fun ArtSpaceScreen(pictureList: ArrayList<PictureData>) {
+    var id: Int by remember { mutableStateOf(0) }
+
+
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -56,8 +134,8 @@ fun ArtSpaceScreen() {
 
             ) {
             Image(
-                painter = painterResource(id = R.drawable.zebra),
-                contentDescription = null,
+                painter = painterResource(id = pictureList[id].imgRes),
+                contentDescription = pictureList[id].title,
                 modifier = Modifier
                     .fillMaxSize()
 
@@ -79,7 +157,7 @@ fun ArtSpaceScreen() {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
-                    text = "Picture Title",
+                    text = pictureList[id].title,
                     fontSize = 20.sp,
                     fontWeight = Bold,
                     textAlign = TextAlign.Justify,
@@ -91,7 +169,7 @@ fun ArtSpaceScreen() {
 
                 )
                 Text(
-                    text = "Artist Name (year)",
+                    text = pictureList[id].artist + " (" + pictureList[id].year + ")",
                     fontSize = 20.sp,
                     textAlign = TextAlign.Justify,
                     modifier = Modifier
@@ -110,12 +188,26 @@ fun ArtSpaceScreen() {
                 .weight(0.15F)
                 .padding(all = 8.dp),
         ) {
-            Button(modifier = Modifier.weight(0.3F).padding(start = 32.dp), onClick = { /*TODO*/ }) {
-                Text(text = "button 1", fontSize = 20.sp)
+            Button(modifier = Modifier
+                .weight(0.3F)
+                .padding(start = 32.dp),
+                onClick = {
+                    println("old id: $id")
+                    id = (id - 1).mod(pictureList.size)
+                    println("new id: $id")
+                }) {
+                Text(text = "Prev", fontSize = 20.sp)
             }
             Spacer(modifier = Modifier.padding(horizontal = 32.dp))
-            Button(modifier = Modifier.weight(0.3F).padding(end = 32.dp),onClick = { /*TODO*/ }) {
-                Text(text = "button 2",fontSize = 20.sp)
+            Button(modifier = Modifier
+                .weight(0.3F)
+                .padding(end = 32.dp),
+                onClick = {
+                    println("old id: $id")
+                    id = (id + 1).mod(pictureList.size)
+                    println("new id: $id")
+                }) {
+                Text(text = "Next", fontSize = 20.sp)
             }
         }
 
@@ -127,6 +219,7 @@ fun ArtSpaceScreen() {
 @Composable
 fun DefaultPreview() {
     ArtSpaceWithComposeTheme {
-        ArtSpaceScreen()
+        val pictureList = populatePictureList()
+        ArtSpaceScreen(pictureList)
     }
 }
